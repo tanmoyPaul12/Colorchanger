09292bcb14dd29ad19a7703667db2bf05ca59ce5
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-08-09 01:33:22 +0530

    done dark light switch pending

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn=document.getElementById("button");
+let randomNumber=()=>{
+    return Math.floor(Math.random()*256);
+};
+let changeColor=()=>{
+    let randomColor=`rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+    // document.body.style.backgroundColor=randomColor;
+};
+button.addEventListener("click", changeColor);
+window.addEventListener("load", changeColor);
